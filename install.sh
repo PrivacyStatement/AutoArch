2891430a8d122c1d7ef19e2b3cefa7f2f6ce03e2
Timon <timon.bentz@gmail.com>
2023-07-05 00:33:31 +0200

    Sprache und Desktop + Grundlagen

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
- 
+
+
+//stelle linux auf Deutsch
+echo LANG=de_DE.UTF-8 > /etc/locale.conf
+echo KEYMAP=de-latin1-nodeadkeys > /etc/vconsole.conf
+ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+//aktivire DHCP für Netzwerk
+systemctl enable dhcpcd
+
+//Update das System
+sudo pacman -Syu
+
+//Treiber für AMD und Desktop
+pacman -S xf89-video-amd plasma plasma-wayland-session
+
+//Aktivire SDDM
+systemctl enable sddm.service
+
+//installiere Applikationen
+pacman -S firefox
