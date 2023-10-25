50ad14d7e8aeb99a8ef8c319c7318049a59c1b85
timon <timon.bentz@gmail.com>
2023-10-25 20:32:18 +0200

    copy file

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,3 @@
-#Bluetooth
-systemctl enable bluetooth --now
-
 #pacman config
 sed -i 's/#\[multilib\]/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
@@ -12,12 +9,15 @@ pacman -Sy ttf-liberation ttf-font-awesome ttf-fira-sans ttf-fira-code ttf-firac
 #driver
 pacman -Sy amdvlk lib32-amdvlk gnome-keyring pipewire pulseaudio-bluetooth
 #hyprland
-pacman -Sy hyprland sddm dunst waybar xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland cliphist wlogout
+pacman -Sy xdg-desktop-portal-hyprland hyprland sddm dunst waybar polkit-kde-agent qt5-wayland qt6-wayland cliphist wlogout
 #comand line applications
-pacman -Sy reflector grim slurp nano flatpak python git bpytop  firewalld ipset ebtables neofetch xautolock swayidle
+pacman -Sy reflector grim slurp nano flatpak python git bpytop firewalld ipset ebtables neofetch xautolock swayidle
 #gui
 pacman -Sy steam alacritty lutris gnome-boxes kdeconnect sxiv mpv vlc xfce4-power-manager thunar lxappearance pavucontrol blueman swappy
- 
+
+#Bluetooth
+systemctl enable bluetooth
+
 #firewall
 systemctl enable firewalld --now
 firewall-cmd --set-default-zone=home
@@ -30,6 +30,7 @@ reflector -c Germany --sort rate -l 50 --save /etc/pacman.d/mirrorlist
 mkdir ./AUR
 cd ./AUR
 git clone https://aur.archlinux.org/yay-git.git
+chmod 777 ./yay-git
 cd ./yay-git
 sudo -u timon makepkg -sic
 cd ../..
@@ -50,18 +51,26 @@ sudo -u timon yay -S swww swaylock-effects goverlay timeshift rofi-wayland archl
 ############dotfiles###########
 ###############################
 
-cp ./files/bashrc ~/.bashrc
-cp ./files/alacitty.yml ~/.config/alacitty.yml
-cp ./files/dunstrc ~/.config/dunstrc
-cp ./files/gtk/gtkrc-2.0 ~/.gtkrc-2.0
-cp ./files/gtk/gtk-3.0 ~/.config/gtk-3.0/settings.ini
-cp ./files/gtk/gtk-4.0 ~/.config/gtk-4.0/settings.ini
-cp ./files/gtk/gtk.sh ~/.config/gtk/gtk.sh
-cp ./files/swaylock ~/.config/swaylock/config
-cp ./files/swappy ~/.config/swappy/config
-cp -r ./background ~/.config/background
-cp ./files/waybar/config ~/.config/waybar/config
-cp ./files/waybar/style.css ~/.config/waybar/style.css
-cp -r ./files/scripts ~/.cofing/scripts
-cp -r ./files/rofi ~/.cofing/rofi
-cp ./files/hyprland.conf ~/.config/hypr/hyprland.conf
+
+
+set_config(){
+    sudo -u timon mkdir $1 
+    sudo -u timon copy $2 "$1/$2"
+}
+
+set_config "~" "./files/bashrc" ".bashrc"
+set_config "~/.config" "./files/alacitty.yml" "alacitty.yml"
+set_config "~/.config" "./files/dunstrc" "dunstrc"
+set_config "~" "./files/gtk/gtkrc-2.0" ".gtkrc-2.0"
+set_config "~/.config/gtk-3.0" "./files/gtk/gtk-3.0" "settings.ini"
+set_config "~/.config/gtk-4.0" "./files/gtk/gtk-4.0" "settings.ini"
+set_config "~/.config/gtk" "./files/gtk/gtk.sh" "gtk.sh"
+set_config "~/.config/swaylock" "./files/swaylock" "config"
+set_config "~/.config/swappy" "./files/swappy" "config"
+set_config "~/.config/waybar" "./files/waybar/config" "config"
+set_config "~/.config/waybar" "./files/waybar/style.css" "style.css"
+set_config "~/.config/hypr" "./files/hyprland.conf" "hyprland.conf"
+sudo -u timon cp -r ./background ~/.config/background
+sudo -u timon cp -r ./files/scripts ~/.cofing/scripts
+sudo -u timon cp -r ./files/rofi ~/.cofing/rofi
+
